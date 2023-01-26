7d82d1389d7ce7012810d83aa41299ad5ec3ba18
Carlos Eduardo <carloseduardogomeslima1@gmail.com>
2023-01-26 10:07:40 -0300

    Produto simples

---
+++ Uri1004.java
@@ -0,0 +1,21 @@
+package Uri;
+
+import java.util.Scanner;
+
+public class Uri1004 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int prod = a * b;
+		
+		System.out.println("PROD = " + prod);
+		
+		sc.close();
+
+	}
+
+}
